52607f9927f66af489eab5f3070a81931eef6ca9
Rohed-Appnap <rohed@appnap.io>
2023-11-04 16:36:13 +0600

    test-20

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           </div>
         ))}
       </section>
-      <p>Landing page with test 18</p>
+      <p>Landing page with test 20</p>
     </main>
   );
 }
