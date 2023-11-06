e9bee17f95f0732f7516d2be50057dbf8f4ad94d
Rohed-Appnap <rohed@appnap.io>
2023-11-06 10:28:16 +0600

    test-24

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import { useGetUser } from "@/repo/api-hooks/useGetUser";
+import CubeBox from './../components/cube/index';
 
 export default function Home() {
   const { data: userData } = useGetUser(1, {});
 
   return (
     <main>
-      <section>
-        {userData?.data.map((user) => (
+      
+        {/* {userData?.data.map((user) => (
           <div key={user.id}>
             {user.id} {user.first_name} {user.last_name}
           </div>
         ))}
       </section>
-      <p>Landing page with test 23</p>
+      <p>Landing page with test 23</p> */}
+      <CubeBox/>
+      <p className="text-[4rem]">Landing page</p>
     </main>
   );
 }
