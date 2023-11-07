177aa639ed959a2349c3b62c799a74e4d9864683
Rohed-Appnap <rohed@appnap.io>
2023-11-07 12:20:57 +0600

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { useGetUser } from "@/repo/api-hooks/useGetUser";
-import CubeBox from './../components/cube/index';
+// import CubeBox from './../components/cube/index';
 
 export default function Home() {
   const { data: userData } = useGetUser(1, {});
 
   return (
     <main>
-      
+      <section> 
         {userData?.data.map((user) => (
           <div key={user.id}>
             {user.id} {user.first_name} {user.last_name}
